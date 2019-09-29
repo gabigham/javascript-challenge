671f4bb231515ff1aa7bf753d679c2bc0259a535
George Bigham <gabigham4@gmail.com>
2019-09-28 19:25:18 -0700

    draft

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,37 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// empty data
+var emptyData = []
+
+// select table from html
+var table = d3.select("#ufo-table");
+var filter = d3.select("#filter-btn")
+
+// update data displayed
+function display(displayedData) {
+  
+  displayedData.forEach((observation) => {
+    var row = table.append("tr");
+    Object.entries(observation).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+}
+
+
+
+
+filter.on("click", function() {
+  var inputDate = d3.select("#datetime");
+  var filterDate = inputDate.property("value");
+  
+  dateFilter = (tableData) => ( tableData.datetime === filterDate )
+  filteredData = tableData.filter(dateFilter)
+  
+  display(filteredData)
+
+})
+
+
